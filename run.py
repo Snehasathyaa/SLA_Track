8d4e42068819c0495917660cbc513b544a2214ab
Snehasathyaa <snehakaliyath@gmail.com>
2026-03-25 10:42:30 +0530

    commit

--- run.py
+++ run.py
@@ -29,6 +29,7 @@ if not os.path.exists('models/sla_classifier.pkl'):
 else:
     print("\n[3/4] Models already trained, skipping...")
 
+
 # 4. Launch Flask
 print("\n[4/4] Starting Flask application...")
 print("\n  ➜ Open http://127.0.0.1:5000")
